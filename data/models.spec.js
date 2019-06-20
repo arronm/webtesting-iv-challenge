5dc6990bb1c1eb46bd1117c06297b3bfe00029e7
Arron M <marshall.arron@gmail.com>
2019-06-20 16:41:42 -0700

    Add test for get() data access layer

--- data/models.spec.js
+++ data/models.spec.js
@@ -10,6 +10,39 @@ describe('models', () => {
     await db('people').truncate();
   });
 
+  describe('get()', () => {
+    it('should get all data if an id is not provided', async () => {
+      await model.add({
+        name: 'a',
+        email: 'a',
+      });
+
+      await model.add({
+        name: 'b',
+        email: 'b',
+      });
+
+      const data = await model.get();
+      expect(data.length).toBe(2);
+    });
+
+    it('should get single person if an id is provided', async () => {
+      await model.add({
+        name: 'a',
+        email: 'a',
+      });
+
+      await model.add({
+        name: 'b',
+        email: 'b',
+      });
+
+      const data = await model.get(2);
+      expect(data.name).toBe('b');
+      expect(data.email).toBe('b');
+    });
+  });
+
   describe('add()', () => {
     it('should insert the provided data', async () => {
       let data = await model.add({
